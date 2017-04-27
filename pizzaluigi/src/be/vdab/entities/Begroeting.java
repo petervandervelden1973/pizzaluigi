31d6c956267663cc2d86f6d8ef09f8d5123ee0b2
Peter van der Velden <petervandervelden@hotmail.com>
2017-04-27 13:12:53 +0200

    dfa

---
+++ pizzaluigi/src/be/vdab/entities/Begroeting.java
@@ -0,0 +1,10 @@
+package be.vdab.entities;
+import java.time.LocalDateTime;
+public class Begroeting {
+	@Override
+	public String toString() {
+		int uur = LocalDateTime.now().getHour();
+		return uur >= 6 && uur < 12 ? "Goedemorgen" :
+			uur >= 12 && uur < 18 ? "Goedemiddag" : "Goedenavond";
+	}
+}
