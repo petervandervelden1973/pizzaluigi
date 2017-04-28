4ed8b1340b67a6c5c482f5feb4ad0e12c3d8eee5
Peter van der Velden <petervandervelden@hotmail.com>
2017-04-28 11:19:03 +0200

    bgb

--- pizzaluigi/src/be/vdab/entities/Persoon.java
+++ pizzaluigi/src/be/vdab/entities/Persoon.java
@@ -6,19 +6,37 @@ public class Persoon {
 	private int aantalKinderen;
 	private boolean gehuwd;
 	
+	public Persoon() {
+			}
+
+	public Persoon(String voornaam, String familienaam, int aantalKinderen, boolean gehuwd) {
+		this.voornaam = voornaam;
+		this.familienaam = familienaam;
+		this.aantalKinderen = aantalKinderen;
+		this.gehuwd = gehuwd;
+	}
 	public String getVoornaam() {
 		return voornaam;
 	}
-		
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
 	public String getFamilienaam() {
 		return familienaam;
 	}
-	
+	public void setFamilienaam(String familienaam) {
+		this.familienaam = familienaam;
+	}
 	public int getAantalKinderen() {
 		return aantalKinderen;
 	}
-	
+	public void setAantalKinderen(int aantalKinderen) {
+		this.aantalKinderen = aantalKinderen;
+	}
 	public boolean isGehuwd() {
 		return gehuwd;
+	}
+	public void setGehuwd(boolean gehuwd) {
+		this.gehuwd = gehuwd;
 	}
 }
