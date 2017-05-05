1d552d14519591b5a0a7adae5f7a4aeb5dc555b9
Peter van der Velden <petervandervelden@hotmail.com>
2017-05-05 15:34:21 +0200

    werre

--- pizzaluigi/src/be/vdab/servlets/PizzasServlet.java
+++ pizzaluigi/src/be/vdab/servlets/PizzasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,6 +18,8 @@ import be.vdab.entities.Pizza;
 public class PizzasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/pizzas.jsp";
+	private static final String PIZZAS_REQUESTS = "pizzasRequests";
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
@@ -27,7 +30,14 @@ public class PizzasServlet extends HttpServlet {
 		pizzas.put(23L, new Pizza(23, "Fungi & Olive", BigDecimal.valueOf(5), false));
 		request.setAttribute("pizzas", pizzas);
 		request.getRequestDispatcher(VIEW).forward(request, response);
-		
-		
+		((AtomicInteger) this.getServletContext().getAttribute(PIZZAS_REQUESTS))
+		.incrementAndGet();
 	}
+	
+	@Override
+	public void init() throws ServletException {
+	this.getServletContext().setAttribute(PIZZAS_REQUESTS, new AtomicInteger());
+	}
+	
+	
 }
---
+++ pizzaluigi/src/be/vdab/servlets/StatistiekServlet.java
@@ -0,0 +1,20 @@
+package be.vdab.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/statistiek.htm")
+public class StatistiekServlet extends HttpServlet {
+private static final long serialVersionUID = 1L;
+private static final String VIEW = "/WEB-INF/JSP/statistiek.jsp";
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+request.getRequestDispatcher(VIEW).forward(request, response);
+}
+}
