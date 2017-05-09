56d16b10f6f682d40aabe9d75d3a471ff7c300bf
Peter van der Velden <petervandervelden@hotmail.com>
2017-05-09 11:42:51 +0200

    ((

--- pizzaluigi/src/be/vdab/servlets/PizzaToevoegenServlet.java
+++ pizzaluigi/src/be/vdab/servlets/PizzaToevoegenServlet.java
@@ -1,41 +1,60 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class PizzaToevoegenServlet
- */
-@WebServlet("/PizzaToevoegenServlet")
+import be.vdab.entities.Pizza;
+import be.vdab.repositories.PizzaRepository;
+import be.vdab.util.StringUtils;
+
+// enkele imports ...
+@WebServlet("/pizzas/toevoegen.htm")
 public class PizzaToevoegenServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PizzaToevoegenServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static final String VIEW = "/WEB-INF/JSP/pizzatoevoegen.jsp";
+	private static final String SUCCESS_VIEW = "/WEB-INF/JSP/pizzas.jsp";
+	private final PizzaRepository pizzaRepository = new PizzaRepository();
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Map<String, String> fouten = new HashMap<>();
+		String naam = request.getParameter("naam");
+		if (!Pizza.isNaamValid(naam)) {
+			fouten.put("naam", "verplicht");
+		}
+		String prijsString = request.getParameter("prijs");
+		BigDecimal prijs = null;
+		if (StringUtils.isBigDecimal(prijsString)) {
+			prijs = new BigDecimal(prijsString);
+			if (!Pizza.isPrijsValid(prijs)) {
+				fouten.put("prijs", "tik een positief getal");
+			}
+		} else {
+			fouten.put("prijs", "tik een getal");
+		}
+		if (fouten.isEmpty()) {
+			boolean pikant = "pikant".equals(request.getParameter("pikant"));
+			pizzaRepository.create(new Pizza(naam, prijs, pikant));
+			request.setAttribute("pizzas", pizzaRepository.findAll());
+			request.getRequestDispatcher(SUCCESS_VIEW).forward(request, response);
+		} else {
+			request.setAttribute("fouten", fouten);
+			request.getRequestDispatcher(VIEW).forward(request, response);
+		}
 	}
-
 }
