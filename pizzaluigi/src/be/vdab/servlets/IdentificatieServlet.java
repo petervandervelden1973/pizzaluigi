993cec14d12bb2e22af8e604891616de956d1970
Peter van der Velden <petervandervelden@hotmail.com>
2017-05-12 14:24:12 +0200

    70

--- pizzaluigi/src/be/vdab/servlets/IdentificatieServlet.java
+++ pizzaluigi/src/be/vdab/servlets/IdentificatieServlet.java
@@ -1,44 +1,87 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/identificatie.htm")
 public class IdentificatieServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/identificatie.jsp";
-	private static final int COOKIE_MAXIMUM_LEEFTIJD = 60 /* seconden */ * 30 /* minuten */;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			String gebruikersnaam = (String) session.getAttribute("gebruikersnaam");
+			if (gebruikersnaam != null) {
+				request.setAttribute("gebruikersnaam", gebruikersnaam);
+			}
+		}
+		request.getRequestDispatcher(VIEW).forward(request, response);
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		Cookie cookie = new Cookie("gebruikersnaam",
-				URLEncoder.encode(request.getParameter("gebruikersnaam"), "UTF-8"));
-		cookie.setMaxAge(COOKIE_MAXIMUM_LEEFTIJD);
-		response.addCookie(cookie);
-		response.sendRedirect(request.getRequestURI());
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		if (request.getCookies() != null) {
-			for (Cookie cookie : request.getCookies()) {
-				if ("gebruikersnaam".equals(cookie.getName())) {
-					request.setAttribute("gebruikersnaam", URLDecoder.decode(cookie.getValue(), "UTF-8"));
-					break;
-				}
-			}
-		}
-		request.getRequestDispatcher(VIEW).forward(request, response);
+		HttpSession session = request.getSession();
+		session.setAttribute("gebruikersnaam", request.getParameter("gebruikersnaam"));
+		response.sendRedirect(request.getRequestURI()); // redirect->huidige
 	}
 }
+
+// package be.vdab.servlets;
+//
+// import java.io.IOException;
+// import java.net.URLDecoder;
+// import java.net.URLEncoder;
+//
+// import javax.servlet.ServletException;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.Cookie;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+//
+// @WebServlet("/identificatie.htm")
+// public class IdentificatieServlet extends HttpServlet {
+// private static final long serialVersionUID = 1L;
+// private static final String VIEW = "/WEB-INF/JSP/identificatie.jsp";
+// private static final int COOKIE_MAXIMUM_LEEFTIJD = 60 /* seconden */ * 30 /*
+// minuten */;
+//
+// @Override
+// protected void doPost(HttpServletRequest request, HttpServletResponse
+// response)
+// throws ServletException, IOException {
+// request.setCharacterEncoding("UTF-8");
+// Cookie cookie = new Cookie("gebruikersnaam",
+// URLEncoder.encode(request.getParameter("gebruikersnaam"), "UTF-8"));
+// cookie.setMaxAge(COOKIE_MAXIMUM_LEEFTIJD);
+// response.addCookie(cookie);
+// response.sendRedirect(request.getRequestURI());
+// }
+//
+// @Override
+// protected void doGet(HttpServletRequest request, HttpServletResponse
+// response)
+// throws ServletException, IOException {
+// if (request.getCookies() != null) {
+// for (Cookie cookie : request.getCookies()) {
+// if ("gebruikersnaam".equals(cookie.getName())) {
+// request.setAttribute("gebruikersnaam", URLDecoder.decode(cookie.getValue(),
+// "UTF-8"));
+// break;
+// }
+// }
+// }
+// request.getRequestDispatcher(VIEW).forward(request, response);
+// }
+// }
