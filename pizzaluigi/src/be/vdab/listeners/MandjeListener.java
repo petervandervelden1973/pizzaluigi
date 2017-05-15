52bfc98909b8ac03553b5357da0ad48f1dbaf340
Peter van der Velden <petervandervelden@hotmail.com>
2017-05-15 10:49:39 +0200

    77

---
+++ pizzaluigi/src/be/vdab/listeners/MandjeListener.java
@@ -0,0 +1,43 @@
+package be.vdab.listeners;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+// enkele imports ...
+@WebListener
+public class MandjeListener implements ServletContextListener, HttpSessionAttributeListener {
+	private static final String MANDJE = "mandje";
+	private static final String AANTAL_MANDJES = "aantalMandjes";
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		event.getServletContext().setAttribute(AANTAL_MANDJES, new AtomicInteger());
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+	}
+
+	@Override
+	public void attributeAdded(HttpSessionBindingEvent event) {
+		if (MANDJE.equals(event.getName())) {
+			((AtomicInteger) event.getSession().getServletContext().getAttribute(AANTAL_MANDJES)).incrementAndGet();
+		}
+	}
+
+	@Override
+	public void attributeRemoved(HttpSessionBindingEvent event) {
+		if (MANDJE.equals(event.getName())) {
+			((AtomicInteger) event.getSession().getServletContext().getAttribute(AANTAL_MANDJES)).decrementAndGet();
+		}
+	}
+
+	@Override
+	public void attributeReplaced(HttpSessionBindingEvent event) {
+	}
+}
