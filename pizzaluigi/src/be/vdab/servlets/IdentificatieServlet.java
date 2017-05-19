047f24f83c09d80f8398a56bdc342038921e0938
Peter van der Velden <petervandervelden@hotmail.com>
2017-05-19 13:40:22 +0200

    81

--- pizzaluigi/src/be/vdab/servlets/IdentificatieServlet.java
+++ pizzaluigi/src/be/vdab/servlets/IdentificatieServlet.java
@@ -30,7 +30,6 @@ public class IdentificatieServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession();
 		session.setAttribute("gebruikersnaam", request.getParameter("gebruikersnaam"));
 		response.sendRedirect(request.getRequestURI()); // redirect->huidige
